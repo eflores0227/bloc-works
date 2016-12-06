d24d413f9deea1ce1b1e0370b8e22bcf0ff18d28
Estefania Flores <eflores0227@gmail.com>
2016-12-05 17:23:54 -0800

    checkpoint-4

--- bloc-works/lib/bloc-works.rb
+++ bloc-works/lib/bloc-works.rb
@@ -5,7 +5,12 @@ require_relative "bloc-works/router"
 module BlocWorks
   class Application
     def call(env)
-          [200, {'Content-Type' => 'text/html'}, ["Hello Blocheads!"]]
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+
+      rack_app = get_rack_app(env)
+      rack_app.call(env)
     end
   end
 end
